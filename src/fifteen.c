33231e156bb0c1d1a125de8d200a3515a49c8569
MaxiPriani <maxipriani2002@gmail.com>
2024-12-13 05:23:49 -0300

    Added project files for Fifteen game

---
+++ src/fifteen.c
@@ -0,0 +1,170 @@
+#include "fifteen.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void clear_screen() {
+    system("clear");
+}
+
+void create_board(int board[ROW][COL]) {
+    int n = 1;
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            if (n <= 15) {
+                board[i][j] = n;
+                n++;
+            } else {
+                board[i][j] = 0;
+            }
+        }
+    }
+}
+
+void empty_tile(int board[ROW][COL], int *empty_row, int *empty_col) {
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            if (board[i][j] == 0) {
+                *empty_row = i;
+                *empty_col = j;
+                return;
+            }
+        }
+    }
+}
+
+void swap(int *a, int *b) {
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void user_movement(int board[ROW][COL], char input) {
+    int row, col;
+    empty_tile(board, &row, &col);
+
+    switch (input) {
+        case 'w':
+            if (row > 0) {
+                swap(&board[row][col], &board[row - 1][col]);
+            }
+            break;
+        case 's':
+            if (row < SIZE - 1) {
+                swap(&board[row][col], &board[row + 1][col]);
+            }
+            break;
+        case 'a':
+            if (col > 0) {
+                swap(&board[row][col], &board[row][col - 1]);
+            }
+            break;
+        case 'd':
+            if (col < SIZE - 1) {
+                swap(&board[row][col], &board[row][col + 1]);
+            }
+            break;
+    }
+}
+
+bool condition(char input) {
+    return (input == 'w' || input == 's' || input == 'a' || input == 'd');
+}
+
+bool is_solved(int board[ROW][COL]) {
+    int n = 1;
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            if (i == ROW - 1 && j == COL - 1) {
+                if (board[i][j] != 0) return false;
+            } else {
+                if (board[i][j] != n) return false;
+                n++;
+            }
+        }
+    }
+    return true;
+}
+
+char get_valid_movement() {
+    char input;
+    printf("Input: ");
+    scanf(" %c", &input);
+
+    while (!condition(input) && input != 'q') {
+        printf("Invalid input, use {'w', 'a', 's', 'd'}\n");
+        printf("Input: ");
+        scanf(" %c", &input);
+    }
+
+    return input;
+}
+
+void print_board(int board[ROW][COL]) {
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            if (board[i][j] == 0) {
+                printf("  _");
+            } else {
+                printf(" %2d ", board[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void shuffle_board(int board[ROW][COL]) {
+    srand(time(NULL));
+
+    for (int i = 0; i < 25; i++) {
+        int row, col;
+        empty_tile(board, &row, &col);
+
+        int movement = rand() % 4;
+        char moves[] = {'w', 's', 'a', 'd'};
+
+        switch (moves[movement]) {
+            case 'w':
+                if (row > 0) user_movement(board, 'w');
+                break;
+            case 's':
+                if (row < SIZE - 1) user_movement(board, 's');
+                break;
+            case 'a':
+                if (col > 0) user_movement(board, 'a');
+                break;
+            case 'd':
+                if (col < SIZE - 1) user_movement(board, 'd');
+                break;
+        }
+    }
+}
+
+int get_difficulty() {
+    Difficulty difficulty;
+
+    printf("=== FIFTEEN ===\n");
+    printf("Select Difficulty:\n");
+    printf("1. EASY (30 movements)\n");
+    printf("2. MEDIUM (20 movements)\n");
+    printf("3. HARD (10 movements)\n");
+    printf("Choice: ");
+    scanf("%d", &difficulty);
+
+    switch (difficulty) {
+        case EASY:
+            difficulty = MAX_MOVES_EASY;
+            break;
+        case MEDIUM:
+            difficulty = MAX_MOVES_MEDIUM;
+            break;
+        case HARD:
+            difficulty = MAX_MOVES_HARD;
+            break;
+        default:
+            difficulty = MAX_MOVES_EASY;
+            break;
+    }
+
+    return difficulty;
+}
---
+++ src/fifteen.h
@@ -0,0 +1,31 @@
+#ifndef FIFTEEN_H
+#define FIFTEEN_H
+
+#include <stdbool.h>
+
+#define ROW 4
+#define COL 4
+#define SIZE 4
+#define MAX_MOVES_EASY 30
+#define MAX_MOVES_MEDIUM 20
+#define MAX_MOVES_HARD 10
+
+typedef enum {
+    EASY = 1,
+    MEDIUM = 2,
+    HARD = 3
+} Difficulty;
+
+void clear_screen();
+void create_board(int board[ROW][COL]);
+void empty_tile(int board[ROW][COL], int *empty_row, int *empty_col);
+void swap(int *a, int *b);
+void user_movement(int board[ROW][COL], char input);
+bool condition(char input);
+bool is_solved(int board[ROW][COL]);
+char get_valid_movement();
+void print_board(int board[ROW][COL]);
+void shuffle_board(int board[ROW][COL]);
+int get_difficulty();
+
+#endif
